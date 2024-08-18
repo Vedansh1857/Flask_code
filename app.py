cf83e4fa446f347189fea8ef8e60f7e9313b5be6
Vedansh1857 <vedanshgupta606@gmail.com>
2024-08-19 01:09:19 +0530

    nothing

--- app.py
+++ app.py
@@ -511,5 +511,9 @@ def fetch_user_details():
     #         connection.close()
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
